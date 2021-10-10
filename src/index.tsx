ccd95a4cba0e7a2e8e8f7e012e461c16c5f741e6
Pierre-Michel Lefebvre <pim.lefebvre@gmail.com>
2021-10-10 16:15:49 +0200

    feat(micro-frontends): Working external modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+(window as any)["renderweb-app-2"] = (containerId: string, props: {}) => {
+  ReactDOM.render(
+    <App {...props} />,
+    document.getElementById(containerId),
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+(window as any)["unmountweb-app-2"] = (containerId: string) => {
+  // @ts-ignore
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
